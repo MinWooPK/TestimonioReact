dc48551fe9291fc106d5e0f6056ff1e47953dc15
MinWooPK <minwoowork91@gmail.com>
2022-12-07 09:55:03 +0100

    Need Css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ function App() {
     <div className="App">
    <div className='contenedor-principal'>
       <h1> Rick Y Morty</h1>
-      <Testimonio />
+      <Testimonio  
+      nombre = 'Perro '
+      feeling = 'Meon'
+      imagen = 'perro'
+      clase = 'Perro'
+      who = 'Morty'
+      testimonio = 'Guau Guau Guau, soy el amigo de Morty'
+      />
+    
+      <Testimonio  
+      nombre = 'Misik '
+      feeling = 'Señor Misik'
+      imagen = 'azul'
+      clase = 'Esclavo'
+      who = 'Rick'
+      testimonio = '¡Que es lo que desea, soy el Señor Misik !'
+      />
+        <Testimonio  
+      nombre = 'Ojete'
+      feeling = 'Personaje Secundario'
+      imagen = 'ojete'
+      clase = 'Random'
+      who = 'Rick y Morty'
+      testimonio = 'No se lo que estoy haciendo, aqui'
+      />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import '../styleSheets/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
     return (
 
         <div className='contendor-testimonio'>
             <img className='imagen-testimonio'
-                src={require('../imagenes/Testimonio-perro.jpeg')}
+                src={require(`../imagenes/Testimonio-${props.imagen}.jpeg`)}
                 alt='foto de perro'
             />
             <div className='contenedor-text-testimonio'>
-                <p className='nombre-testimonio'>Perro Rick y Morty</p>
-                <p className='cargo-testimonio'>Perro de Morty</p>
-                <p className='texto-testimonio'> guau guauuu guauu</p>
+                <p className='nombre-testimonio'>
+                    <strong>{props.nombre} </strong>,{props.feeling}</p>
+                <p className='cargo-testimonio'>{props.clase} de <strong>{props.who}</strong></p>
+                <p className='texto-testimonio'> "{props.testimonio}"</p>
             </div>
 
         </div>
